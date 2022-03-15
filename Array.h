57d927ba3a30ca72b8bc2fb7dde2957b07d005c4
George_Illarionov <george.practice@mail.ru>
2022-03-15 16:17:52 +0300

    Array class

---
+++ Array.h
@@ -0,0 +1,112 @@
+#pragma once
+
+#include <cassert>
+
+template<typename T>
+class Array
+{
+public:
+    // Конструкоторы
+    Array<T>() : arr(nullptr), size(0), capacity(0) {}
+    Array<T>(size_t size_) : size(size_), capacity(size_) { arr = new T(size_); }
+
+    // Функции
+    void pushBack(T elem);
+    T& operator [](size_t index);
+
+    // Деструктор
+    ~Array<T>();
+
+    //Перенос
+    Array<T>(Array<T>&& srcArr);
+    Array<T>& operator = (Array<T>&& srcArr);
+
+    //Копирование
+    Array<T>(const Array<T>& srcArr);
+
+private:
+    void extendArray();
+
+private:
+    T* arr;
+    size_t size;
+    size_t capacity;
+};
+
+
+// Деструктор
+template<typename T>
+Array<T>::~Array()
+{
+    delete arr;
+    arr = nullptr;
+    size = capacity = 0;
+}
+
+// Копирование
+template<typename T>
+Array<T>::Array(const Array<T>& srcArr)
+{
+    this->size = srcArr.size;
+    this->capacity = srcArr.capacity;
+    this->arr = new T(this->capacity);
+
+    for (size_t i = 0; i < size; i++)
+    {
+        this->arr[i] = srcArr.arr[i];
+    }
+}
+
+//Перенос
+template<typename T>
+Array<T>& Array<T>::operator=(Array<T>&& srcArr)
+{
+    if (&srcArr == this)
+        return *this;
+
+    ~Array();
+
+    this->size = srcArr.size;
+    this->arr = srcArr.arr;
+    this->capacity = srcArr.capacity;
+
+    srcArr.size = srcArr.capacity = 0;
+
+    return *this;
+}
+
+template<typename T>
+Array<T>::Array(Array<T>&& srcArr)
+{
+    this->arr = srcArr.arr;
+    this->size = srcArr.size;
+    this->capacity = srcArr.capacity;
+
+    srcArr.arr = nullptr;
+    srcArr.capacity = srcArr.size = 0;
+}
+
+// Функции
+template<typename T>
+void Array<T>::pushBack(T elem)
+{
+    if (size == capacity)
+        extendArray();
+    arr[size++] = elem;
+}
+
+template<typename T>
+T& Array<T>::operator[](size_t index)
+{
+    assert(index >= 0 && index < size);
+    return arr[index];
+}
+
+template<typename T>
+void Array<T>::extendArray()
+{
+    T* newArr = new T(capacity = capacity ? capacity *= 2 : 1);
+    if (arr)
+        delete arr;
+    arr = newArr;
+}
--- main.cpp
+++ main.cpp
@@ -1,6 +1,67 @@
-﻿#include <iostream>
+﻿//2_4.Дан отсортированный массив различных целых чисел A[0..n - 1] 
+//и массив целых чисел B[0..m - 1].Для каждого элемента массива B[i] 
+//найдите минимальный индекс элемента массива A[k], ближайшего по значению к B[i].
+//Требования: Время работы поиска для каждого элемента B[i] : O(log(k)).
+//Внимание!В этой задаче для каждого B[i] сначала нужно определить диапазон для бинарного
+//поиска размером порядка k с помощью экспоненциального поиска, а потом уже в нем делать 
+//бинарный поиск.
+//
+//n ≤ 110000, m ≤ 1000.
+
+
+#include <iostream>
+
+#include "Array.h"
+using namespace std;
+
+
+//int findClosestElementInArr(vector<int>& arr, int val);
+//void expSearch(int& startIndex, int& endIndex, const vector<int>& arr, const int val);
+//int binarySearch(const vector<int>& arr, const int val, const int startIndex, const int endIndex);
 
 int main()
 {
+    //b = f();
+    //int count, val;
+
+    //vector<int> A;
+    //cin >> count;
+    //for (int i = 0; i < count; i++)
+    //{
+    //    cin >> val;
+    //    A.push_back(val);
+    //}
+
+    //vector<int> B;
+    //cin >> count;
+    //for (int i = 0; i < count; i++)
+    //{
+    //    cin >> val;
+    //    B.push_back(val);
+    //}
+
+    //for (int i = 0; i < B.size(); i++)
+    //{
+    //    cout << findClosestElementInArr(A, B[i]);
+    //}
+
+
     return EXIT_SUCCESS;
 }
+
+//int findClosestElementInArr(const vector<int>& arr, const int val)
+//{
+//    int startIndex, endIndex;
+//    expSearch(startIndex, endIndex, arr, val);
+//    return binarySearch(arr, val, startIndex, endIndex);
+//}
+//
+//void expSearch(int& startIndex, int& endIndex, const vector<int>& arr, const int val)
+//{
+//
+//}
+//
+//int binarySearch(const vector<int> &arr, const int val, const int startIndex, const int endIndex)
+//{
+//
+//}
