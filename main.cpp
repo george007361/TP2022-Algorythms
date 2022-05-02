aaf781ef384864d808db536a569bcf8b900a238e
George_Illarionov <george.practice@mail.ru>
2022-05-02 12:53:02 +0300

    Style fix

--- main.cpp
+++ main.cpp
@@ -17,186 +17,188 @@ template<typename T, typename Comparator>
 class BinaryTree
 {
 public:
-    explicit BinaryTree(const Comparator& comp) : cmp(comp), root(nullptr) {};
-    ~BinaryTree();
+	explicit BinaryTree(const Comparator& comp) : cmp(comp), root(nullptr) {};
+	~BinaryTree();
 
-    BinaryTree(const BinaryTree&) = delete;
-    BinaryTree& operator =(const BinaryTree&) = delete;
-    BinaryTree(BinaryTree&&);
-    BinaryTree& operator = (BinaryTree&&);
+	BinaryTree(const BinaryTree&) = delete;
+	BinaryTree& operator =(const BinaryTree&) = delete;
+	BinaryTree(BinaryTree&&);
+	BinaryTree& operator = (BinaryTree&&);
 
-    void insert(const T& key);
-    void postOrderPrint() const;
+	void insert(const T& key);
+	void postOrderPrint() const;
 
 
 private:
-    struct BinaryTreeNode
-    {
-        T key;
-        BinaryTreeNode* parent;
-        BinaryTreeNode* left;
-        BinaryTreeNode* right;
-        BinaryTreeNode(const T& key_, BinaryTreeNode *parent_) : key(key_), left(nullptr), right(nullptr), parent(parent_) {}
-        ~BinaryTreeNode()
-        {
-            if (left) 
-            {
-                delete left;
-                left = nullptr;
-            }
-            if (right)
-            {
-                delete right;
-                right = nullptr;
-            }
-            parent = nullptr;
-        }
-    };
+	struct BinaryTreeNode
+	{
+		T key;
+		BinaryTreeNode* parent;
+		BinaryTreeNode* left;
+		BinaryTreeNode* right;
+		BinaryTreeNode(const T& key_, BinaryTreeNode* parent_) : key(key_), left(nullptr), right(nullptr), parent(parent_) {}
+		~BinaryTreeNode()
+		{
+			if (left)
+			{
+				delete left;
+				left = nullptr;
+			}
+			if (right)
+			{
+				delete right;
+				right = nullptr;
+			}
+			parent = nullptr;
+		}
+	};
 
-    BinaryTreeNode* root;
-    Comparator cmp;
+	BinaryTreeNode* root;
+	Comparator cmp;
 };
 
 template<typename T, typename Comparator>
 BinaryTree<T, Comparator>::~BinaryTree()
 {
-    if (root)
-    {
-        delete root;
-        root = nullptr;
-    }
+	if (root)
+	{
+		delete root;
+		root = nullptr;
+	}
 }
 
 template<typename T, typename Comparator>
 BinaryTree<T, Comparator>::BinaryTree(BinaryTree&& src)
 {
-    root = src.root;
-    cmp = src.cmp;
+	root = src.root;
+	cmp = src.cmp;
 
-    src.root = nullptr;
+	src.root = nullptr;
 }
 
 template<typename T, typename Comparator>
 BinaryTree<T, Comparator>& BinaryTree<T, Comparator>::operator=(BinaryTree&& src)
 {
-    if (this == &src)
-    {
-        return *this;
-    }
-    ~BinaryTree();
+	if (this == &src)
+	{
+		return *this;
+	}
+	~BinaryTree();
 
-    root = src.root;
-    cmp = src.cmp;
+	root = src.root;
+	cmp = src.cmp;
 
-    src.root = nullptr;
+	src.root = nullptr;
 
-    return *this;
+	return *this;
 }
 
 template<typename T, typename Comparator>
 void BinaryTree<T, Comparator>::insert(const T& key)
 {
-    BinaryTreeNode *newNode = new BinaryTreeNode(key, nullptr);
-    if (root == nullptr)
-    {
-        root = newNode;
-        return;
-    }
+	BinaryTreeNode* newNode = new BinaryTreeNode(key, nullptr);
+	if (root == nullptr)
+	{
+		root = newNode;
+		return;
+	}
 
-    BinaryTreeNode* head = root;
-    while (head != nullptr)
-    {
-        if (cmp(newNode -> key, head->key) > 0)
-        {
-            if (head->right != nullptr)
-            {
-                head = head->right;
-            }
-            else
-            {
-                newNode->parent = head;
-                head->right = newNode;
-                break;
-            }
-        }
-        else
-        {
-            if (head->left != nullptr)
-            {
-                head = head->left;
-            }
-            else
-            {
-                newNode->parent = head;
-                head->left = newNode;
-                break;
-            }
-        }
-    }
+	BinaryTreeNode* head = root;
+	while (head != nullptr)
+	{
+		if (cmp(newNode->key, head->key) > 0)
+		{
+			if (head->right != nullptr)
+			{
+				head = head->right;
+			}
+			else
+			{
+				newNode->parent = head;
+				head->right = newNode;
+				break;
+			}
+		}
+		else
+		{
+			if (head->left != nullptr)
+			{
+				head = head->left;
+			}
+			else
+			{
+				newNode->parent = head;
+				head->left = newNode;
+				break;
+			}
+		}
+	}
 }
 
 template<typename T, typename Comparator>
 void BinaryTree<T, Comparator>::postOrderPrint() const
 {
-    if (!root)
-    {
-        return;
-    }
+	if (!root)
+	{
+		return;
+	}
 
-    BinaryTreeNode* head = root;
-    stack<BinaryTreeNode *> st;
-    st.push(head);
+	BinaryTreeNode* head = root;
+	stack<BinaryTreeNode*> st;
+	st.push(head);
 
-    while (!st.empty()) {
-        BinaryTreeNode *next = st.top();
+	while (!st.empty()) {
+		BinaryTreeNode* next = st.top();
 
-        bool finishedSubtrees = (next->right == head || next->left == head);
-        bool isLeaf = (next->left == nullptr && next->right == nullptr);
-        if (finishedSubtrees || isLeaf) 
-        {
-            st.pop();
-            cout << (next->key) << " ";
-            head = next;
-        }
-        else 
-        {
-            if (next->right != nullptr) {
-                st.push(next->right);
-            }
-            if (next->left != nullptr) {
-                st.push(next->left);
-            }
-        }
-    }
+		bool finishedSubtrees = (next->right == head || next->left == head);
+		bool isLeaf = (next->left == nullptr && next->right == nullptr);
+		if (finishedSubtrees || isLeaf)
+		{
+			st.pop();
+			cout << (next->key) << " ";
+			head = next;
+		}
+		else
+		{
+			if (next->right != nullptr)
+			{
+				st.push(next->right);
+			}
+			if (next->left != nullptr)
+			{
+				st.push(next->left);
+			}
+		}
+	}
 }
 
 
 class IntComparator
 {
 public:
-    int operator() (const int& a, const int& b)
-    {
-        return a - b;
-    }
+	int operator() (const int& a, const int& b)
+	{
+		return a - b;
+	}
 };
 
 int main()
 {
 
-    IntComparator cmp;
-    BinaryTree<int, IntComparator> bt(cmp);
+	IntComparator cmp;
+	BinaryTree<int, IntComparator> bt(cmp);
 
-    int N;
-    cin >> N;
-    assert(N > 0);
-    for (int i = 0; i < N; ++i)
-    {
-        int elem;
-        cin >> elem;
-        bt.insert(elem);
-    }
+	int N;
+	cin >> N;
+	assert(N > 0);
+	for (int i = 0; i < N; ++i)
+	{
+		int elem;
+		cin >> elem;
+		bt.insert(elem);
+	}
 
-    bt.postOrderPrint();
+	bt.postOrderPrint();
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
